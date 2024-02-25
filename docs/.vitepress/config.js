99d9d1fc84178169f7bc8400b2acb05fe7275075
Wemsur <153404985+Wemsur@users.noreply.github.com>
2024-02-25 10:46:28 +0800

    Add files via upload

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+// .vitepress/config.js
+
+export default {
+  // 网页设置
+  lang: 'zh-cmn-Hans',
+  title: 'Wemsur的个人主页',
+  description: 'b站UP Wemsur的个人主页',
+  head: [['link', { rel: 'icon', href: '/favicon.jpg' }]],
+  base: '/',
+  outDir: '../public',
+
+  themeConfig: {
+    // theme-level options
+    logo: '/favicon.jpg',
+    nav: [
+      { text: 'QQ', link: 'https://qm.qq.com/q/m3LNoDjY0U' },
+      { text: 'bilibili', link: 'https://space.bilibili.com/1826712095' },
+      { text: 'Github', link: 'https://github.com/Wemsur' }
+    ]
+  }
+}
+
+
+
